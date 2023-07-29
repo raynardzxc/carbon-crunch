552c8c17714ac1977bb690a742b10a660d7fa2c2
raynardzxc <43593119+raynardzxc@users.noreply.github.com>
2023-07-29 22:14:34 +0800

    added upgrades
    battery not working
    upgrade logic not working

--- carboncrunch/modules/game/gameModule.R
+++ carboncrunch/modules/game/gameModule.R
@@ -132,8 +132,39 @@ game_server <- function(id) {
       battery_value <- reactiveVal(15)
       battery_cap <- reactiveVal(15)
       sunlight <- reactiveVal(rgamma(1, shape = 2, scale = 3.5)) # This would give you a mean of 7 and a variance of 14.
+      
+      battery_level <- reactiveVal(1)
+      pl_levels <- reactiveVal(rep(1,5))
+      
       selected_component <- reactiveVal("None")
       summary_data <- reactiveVal() 
+      
+      # fixed values
+      battery_df <- data.frame(level = 1:3,
+                               capacity = c(10, 15, 20),
+                               cost = c(0, 10, 20))
+      
+      pl_df <- data.frame(level = 1:3,
+                          cash_generated = c(10, 15, 30),
+                          emissions = c(10, 15, 20),
+                          solar_consumption = c(2, 5, 10),
+                          cost = c(0, 15, 30))
+      
+      energy_per_line <- 2  # energy required per production line
+      
+      # Check if the battery value is sufficient
+      battery_is_sufficient <- reactive({
+        battery_value() >= total_required_energy()
+      })
+      
+      # lookup the stats according to the level
+      battery_stats <- reactive({
+        battery_df[battery_df$level == battery_level(),]
+      })
+      
+      pl_stats <- reactive({
+        pl_df[pl_df$level == pl_levels()[1],]
+      })
       
       resetGame <- function() {
         day(1)
@@ -147,13 +178,126 @@ game_server <- function(id) {
       }
       
       generateUI <- function(name) {
-        div(
-          h3(paste0(name, " Selected")),
-          p(paste0("Some descriptive text about ", name, "...")),
-          actionButton(ns(paste0("button_", name, "_1")), paste0("Button 1 for ", name)),
-          actionButton(ns(paste0("button_", name, "_2")), paste0("Button 2 for ", name))
-        )
+        if (name == "Battery") {
+          cur_battery_stats <- battery_stats()
+          next_battery_stats <- if (battery_level() < 3) {
+            battery_df[battery_df$level == battery_level() + 1,]
+          } else {
+            NULL
+          }
+          upgrade_cost <- if (!is.null(next_battery_stats)) {
+            next_battery_stats$cost
+          } else {
+            NA
+          }
+          
+          # Generate current battery stats HTML
+          cur_battery_html <- paste0(
+            "<tr>",
+            "<td>", "Current Capacity: ", "</td>",
+            "<td>", cur_battery_stats()$capacity, "</td>",
+            "</tr>"
+          )
+          
+          # Generate next level battery stats HTML if available
+          next_battery_html <- if (!is.null(next_battery_stats)) {
+            paste0(
+              "<tr>",
+              "<td>", "Next Level Capacity: ", "</td>",
+              "<td>", next_battery_stats$capacity, "</td>",
+              "</tr>",
+              "<tr>",
+              "<td>", "Upgrade Cost: ", "</td>",
+              "<td>", next_battery_stats$cost, "</td>",
+              "</tr>"
+            )
+          }
+          
+          div(
+            h3(paste0(name, " Selected")),
+            HTML(
+              paste0(
+                '<table style="width:100%; border: none;">',
+                cur_battery_html,
+                next_battery_html,
+                '</table>'
+              )
+            ),
+            if (!is.null(next_battery_stats)) {
+              actionButton(ns(paste0("upgrade_", name)), "Upgrade")
+            }
+          )
+          
+        } else if (startsWith(name, "Production Line")) {
+          pl_index <- as.integer(substr(name, nchar(name), nchar(name)))
+          cur_pl_stats <- pl_df[pl_df$level == pl_levels()[pl_index],]
+          next_pl_stats <- if (pl_levels()[pl_index] < 3) {
+            pl_df[pl_df$level == pl_levels()[pl_index] + 1,]
+          } else {
+            NULL
+          }
+          upgrade_cost <- if (!is.null(next_pl_stats)) {
+            next_pl_stats$cost
+          } else {
+            NA
+          }
+          
+          # Generate current production line stats HTML
+          cur_pl_html <- paste0(
+            "<tr>",
+            "<td>", "Current Cash Generated: ", "</td>",
+            "<td>", cur_pl_stats$cash_generated, "</td>",
+            "</tr>",
+            "<tr>",
+            "<td>", "Current Emissions: ", "</td>",
+            "<td>", cur_pl_stats$emissions, "</td>",
+            "</tr>",
+            "<tr>",
+            "<td>", "Current Solar Consumption: ", "</td>",
+            "<td>", cur_pl_stats$solar_consumption, "</td>",
+            "</tr>"
+          )
+          
+          # Generate next level production line stats HTML if available
+          next_pl_html <- if (!is.null(next_pl_stats)) {
+            paste0(
+              "<tr>",
+              "<td>", "Next Level Cash Generated: ", "</td>",
+              "<td>", next_pl_stats$cash_generated, "</td>",
+              "</tr>",
+              "<tr>",
+              "<td>", "Next Level Emissions: ", "</td>",
+              "<td>", next_pl_stats$emissions, "</td>",
+              "</tr>",
+              "<tr>",
+              "<td>", "Next Level Solar Consumption: ", "</td>",
+              "<td>", next_pl_stats$solar_consumption, "</td>",
+              "</tr>",
+              "<tr>",
+              "<td>", "Upgrade Cost: ", "</td>",
+              "<td>", next_pl_stats$cost, "</td>",
+              "</tr>"
+            )
+          }
+          
+          div(
+            h3(paste0(name, " Selected")),
+            HTML(
+              paste0(
+                '<table style="width:100%; border: none;">',
+                cur_pl_html,
+                next_pl_html,
+                '</table>'
+              )
+            ),
+            if (!is.null(next_pl_stats)) {
+              actionButton(ns(paste0("upgrade_", name)), "Upgrade")
+            }
+          )
+        }
       }
+      
+      
       
       # Helper function to round if numeric
       round_if_numeric <- function(x) {
@@ -250,13 +394,20 @@ game_server <- function(id) {
         }
       })
       
-      
       output$next_day_button <- renderUI({
-        if (day() < 30) {
+        if (day() < 30 && battery_is_sufficient()) {
           PrimaryButton.shinyInput(
             inputId = ns("next_day"),
             class=".btn",
-            text="Next Day"
+            text="Next Day",
+            disabled=FALSE # Ensure button is enabled again
+          )
+        } else if (!battery_is_sufficient()) {
+          PrimaryButton.shinyInput(
+            inputId = ns("next_day"),
+            class=".btn",
+            text="Next Day",
+            disabled = TRUE  # Disable the button
           )
         } else {
           PrimaryButton.shinyInput(
@@ -267,6 +418,17 @@ game_server <- function(id) {
         }
       })
       
+      # Compute the total required solar energy
+      total_required_energy <- reactive({
+        total_energy_needed = 0
+        for (i in 1:5) {
+          toggleValue <- input[[paste0("toggle", i)]]
+          if (toggleValue == FALSE) {  # the production line is using solar energy
+            total_energy_needed = total_energy_needed + energy_per_line
+          }
+        }
+        total_energy_needed
+      })
       
       observeEvent(input$battery, {
         selected_component("Battery")
@@ -308,20 +470,45 @@ game_server <- function(id) {
         
         # Apply updates
         day(day() + 1)
-        for(i in 1:5) {
-          toggleValue <- input[[paste0("toggle", i)]]
-          if(toggleValue == FALSE) {
-            cash(cash() + 10) # Cash added
-            battery_value(battery_value()-2) # Battery amount used
-            updateProgressBar(
-              session = session,
-              id = ns("battery_bar"),
-              value = battery_value()
-            )
-          } else if(toggleValue == TRUE) {
-            cash(cash() + 15) # Cash added
-            emissions(emissions() + 10) # Emissions generated
-          }
+        # Production Line 1
+        if(input$toggle1 == FALSE) {
+          cash(cash() + 10) # Cash added
+          battery_value(battery_value()-2) # Battery amount used
+        } else if(input$toggle1 == TRUE) {
+          cash(cash() + 15) # Cash added
+          emissions(emissions() + 10) # Emissions generated
+        }
+        # Production Line 2
+        if(input$toggle2 == FALSE) {
+          cash(cash() + 10) # Cash added
+          battery_value(battery_value()-2) # Battery amount used
+        } else if(input$toggle2 == TRUE) {
+          cash(cash() + 15) # Cash added
+          emissions(emissions() + 10) # Emissions generated
+        }
+        # Production Line 3
+        if(input$toggle3 == FALSE) {
+          cash(cash() + 10) # Cash added
+          battery_value(battery_value()-2) # Battery amount used
+        } else if(input$toggle3 == TRUE) {
+          cash(cash() + 15) # Cash added
+          emissions(emissions() + 10) # Emissions generated
+        }
+        # Production Line 4
+        if(input$toggle4 == FALSE) {
+          cash(cash() + 10) # Cash added
+          battery_value(battery_value()-2) # Battery amount used
+        } else if(input$toggle4 == TRUE) {
+          cash(cash() + 15) # Cash added
+          emissions(emissions() + 10) # Emissions generated
+        }
+        # Production Line 5
+        if(input$toggle5 == FALSE) {
+          cash(cash() + 10) # Cash added
+          battery_value(battery_value()-2) # Battery amount used
+        } else if(input$toggle5 == TRUE) {
+          cash(cash() + 15) # Cash added
+          emissions(emissions() + 10) # Emissions generated
         }
         
         # Add battery from sunlight
@@ -391,8 +578,6 @@ game_server <- function(id) {
         change_page("/")
         print("Back to home")
       })
-      
-      
     }
   )
 }
