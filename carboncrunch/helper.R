ec36a9083a4cb17ba6142dd09790de5c6681f4a2
weixian-nwx <ng.wei.xian.formal@gmail.com>
2023-07-31 18:42:41 +0800

    battery assets

--- carboncrunch/helper.R
+++ carboncrunch/helper.R
@@ -79,3 +79,78 @@ getLineInfo <- function(){
   result
 }
 # ------------------------------------------------------------------------------------
+
+## Function for login
+#Generate, or re-generate, HTML to create modal dialog for Password creation
+passwordModal <- function(failed = FALSE) {
+  modalDialog(
+    title = "Create a new password",
+    textInput("username1", "Enter your desired username:"),
+    passwordInput("password1", "Enter a new password:"),
+    passwordInput("password2", "Confirm by re-entering the new password:"),
+    
+    if (failed)
+      div(tags$b("The passwords do not match. Try again.", style = "color: red;")),
+    
+    footer = tagList(
+      modalButton("Cancel"),
+      actionButton("passwordok", "OK")
+    )
+  )
+}
+
+loginModal <- function(failed = FALSE) {
+  modalDialog(
+    title = "Login",
+    textInput("playername", "Enter your username", "Username"),
+    passwordInput("password3", "Enter your password:", "Password"),
+    if (failed)
+      div(tags$b("There is no registered player with that name and password. Try again or re-register.", style = "color: red;")),
+    
+    footer = tagList(
+      modalButton("Cancel"),
+      actionButton("loginok", "OK")
+    )
+  )
+}
+
+createNewPlayerQuery <- function(conn,playername,password){
+  # playername or password could contain an SQL insertion attack
+  # Create a template for the query with placeholder for  password
+  querytemplate <- "INSERT INTO LeaderPlayer (playername,password) VALUES (?id1,?id2);"
+  query<- sqlInterpolate(conn, querytemplate,id1=playername,id2=password)
+}
+
+registerPlayer <- function(playername, password){
+  #open the connection
+  conn <- getAWSConnection()
+  query <- createNewPlayerQuery(conn,playername,password)
+  print(query) #for debug
+  
+  success <- FALSE
+  iter <- 0
+  MAXITER <- 5
+  while(!success & iter < MAXITER){
+    iter <- iter+1
+    tryCatch(
+      
+      {  # This is not a SELECT query so we use dbExecute
+        result <- dbExecute(conn,query)
+        print(result)
+        success <- TRUE
+      }, error=function(cond){print("registerPlayer: ERROR")
+        print(cond)
+        # The query failed, likely because of a duplicate playername
+        query <- createNewPlayerQuery(conn,playername,password) }, 
+      warning=function(cond){print("registerPlayer: WARNING")
+        print(cond)},
+      finally = {print(paste0("Iteration ",iter," done."))
+      }
+    )
+  } # end while loop
+  # This may not have been successful
+  if (!success) playername = NULL
+  #Close the connection
+  dbDisconnect(conn)
+  playername
+}
