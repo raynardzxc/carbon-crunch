7e9e5524a63cda39132cf49cfb094b94b21d1968
weixian-nwx <ng.wei.xian.formal@gmail.com>
2023-08-01 16:24:05 +0800

    added new menu buttons
    not yet implemented

--- carboncrunch/app.R
+++ carboncrunch/app.R
@@ -28,6 +28,7 @@ home_page <- div(
     column(12,  ## Column might be redundant here 
            align="center",
            h1("Welcome to Carbon Crunch", class = "title-text"),
+           
            PrimaryButton.shinyInput(
              "tutorial",
              class="home-button",
